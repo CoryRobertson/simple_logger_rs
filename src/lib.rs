2136e2dd581d8a106d9b48b5c440e498b34ec541
coryrobertson <cory.robertson17@gmail.com>
2022-11-05 17:26:52 -0700

    cargo fmt changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-use std::fs::{OpenOptions};
+use chrono::{DateTime, Datelike, Local, Timelike};
+use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
 use std::string::ToString;
 use std::sync::Mutex;
-use chrono::{Datelike, DateTime, Local, Timelike};
 
 pub static FILE_NAME: Mutex<&str> = Mutex::new("log.log");
 
@@ -16,59 +16,68 @@ pub enum LogLevel {
 impl LogLevel {
     fn to_string(&self) -> String {
         match self {
-            LogLevel::Error => {"Error".to_string()}
-            LogLevel::Warning => {"Warn".to_string()}
-            LogLevel::Log => {"Log".to_string()}
+            LogLevel::Error => "Error".to_string(),
+            LogLevel::Warning => "Warn".to_string(),
+            LogLevel::Log => "Log".to_string(),
         }
     }
 }
 
 pub fn log(text: &str, level: LogLevel) {
-
-    let path = {
-        Path::new( *FILE_NAME.lock().unwrap())
-    };
+    let path = { Path::new(*FILE_NAME.lock().unwrap()) };
     let display = path.display();
     let date: DateTime<Local> = Local::now();
 
-    // let mut file = match File::create(&path) {
-    //     Ok(f) => {f}
-    //     Err(e) => {panic!("{}, {}",e, display);}
-    // };
-    let file_name = {
-        *FILE_NAME.lock().unwrap()
-    };
+    let file_name = { *FILE_NAME.lock().unwrap() };
 
     let mut file = match OpenOptions::new()
         .write(true)
         .create(true)
         .append(true)
-        .open(file_name) {
-        Ok(f) => {f}
-        Err(e) => {panic!("{}, {}", e, display);}
+        .open(file_name)
+    {
+        Ok(f) => f,
+        Err(e) => {
+            panic!("{}, {}", e, display);
+        }
     };
 
     let am_pm = match date.hour12().0 {
-        true => {"PM"}
-        false => {"AM"}
+        true => "PM",
+        false => "AM",
     };
 
-    let time_format = format!("{}:{:02}:{:02}{}", date.hour12().1, date.minute(), date.second(), am_pm);
+    let time_format = format!(
+        "{}:{:02}:{:02}{}",
+        date.hour12().1,
+        date.minute(),
+        date.second(),
+        am_pm
+    );
 
-    let full_text = format!("[{}]\t[{}-{}-{}: {}]\t{} \n", level.to_string(), date.year(), date.month(), date.day(), time_format, text);
+    let full_text = format!(
+        "[{}]\t[{}-{}-{}: {}]\t{} \n",
+        level.to_string(),
+        date.year(),
+        date.month(),
+        date.day(),
+        time_format,
+        text
+    );
 
     match file.write_all(full_text.as_bytes()) {
         Ok(_) => {}
-        Err(e) => {panic!("{}, {}",e, display);}
+        Err(e) => {
+            panic!("{}, {}", e, display);
+        }
     };
 }
 
-
 #[cfg(test)]
 mod tests {
+    use super::*;
     use std::thread::sleep;
     use std::time::Duration;
-    use super::*;
 
     #[test]
     fn log_test_log() {
@@ -95,6 +104,4 @@ mod tests {
         log("this is a different log file test", LogLevel::Error);
         *FILE_NAME.lock().unwrap() = "log.log";
     }
-
-
 }
